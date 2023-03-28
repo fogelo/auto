765cc7dbd5d6016acda02edcad5d9a9b09d69889
Орлов Антон <fogelo@yandex.ru>
2023-03-28 14:21:52 +0700

    подключены стили (wp_head)

--- functions.php
+++ functions.php
@@ -3,9 +3,11 @@
 add_action( 'wp_enqueue_scripts', 'auto_scripts' );
 // add_action('wp_print_styles', 'theme_name_scripts'); // можно использовать этот хук он более поздний
 function auto_scripts() {
-	wp_enqueue_style( 'style-name', get_stylesheet_uri() );
-
-
+	wp_enqueue_style( 'style.css', get_template_directory_uri() . '/assets/css/style.css'); //точка это конкатенация
+	
+    //get_template_directory_uri() - путь до текущей папки в которой лежит functions.php. 
 	// wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 };
 ?>
+
+
